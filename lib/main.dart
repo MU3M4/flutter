0fdbd0bc49ec4adb96d7c56ecec93a1155be70fe
Emmanuel Muema <emmanuelmuemam@gmail.com>
2022-05-02 13:08:41 +0300

    step-2

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 import 'package:flutter_progress/views/login_view.dart';
 import 'package:flutter_progress/views/register_view.dart';
+import 'package:flutter_progress/views/verify_email_view.dart';
 import 'firebase_options.dart';
 
 void main() {
@@ -34,14 +36,19 @@ class HomePage extends StatelessWidget {
       builder: (context, snapshot) {
         switch (snapshot.connectionState) {
           case ConnectionState.done:
-            // final user = FirebaseAuth.instance.currentUser;
-            // //final emailVerified = user?.emailVerified ?? false;
-            // if (user?.emailVerified ?? false) {
-            //   return const Text("done");
-            // } else {
-            //   return const VerifyEmailView();
-            // };
-            return const LoginView();
+            final user = FirebaseAuth.instance.currentUser;
+            if (user != null) {
+              if (user.emailVerified) {
+                print('Email is verified');
+              } else {
+                return const VerifyEmailView();
+              }
+            } else {
+              return const LoginView();
+            }
+            return const Text("done");
+            
+           
           default:
             return const CircularProgressIndicator();
         }
@@ -49,4 +56,3 @@ class HomePage extends StatelessWidget {
     );
   }
 }
-
--- lib/views/login_view.dart
+++ lib/views/login_view.dart
@@ -91,6 +91,12 @@ class _LoginViewState extends State<LoginView> {
                 },
                 child: const Text("Login"),
               ),
+              TextButton(
+                  onPressed: () {
+                    Navigator.of(context).pushNamedAndRemoveUntil(
+                        '/register/', (route) => false);
+                  },
+                  child: const Text("Not Registered? Register Here!"))
             ],
           );
           //default:return const Text('Loading...');
