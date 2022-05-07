119e77432104d63ec0d2132a3bab4b4f412f5daa
Emmanuel Muema <emmanuelmuemam@gmail.com>
2022-05-07 21:27:27 +0300

    step 2

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-
+import 'dart:developer' as devtools show log;
 import 'package:flutter_progress/views/login_view.dart';
 import 'package:flutter_progress/views/register_view.dart';
 import 'package:flutter_progress/views/verify_email_view.dart';
@@ -43,16 +43,86 @@ class HomePage extends StatelessWidget {
               } else {
                 return const VerifyEmailView();
               }
-            } else {
-              return const LoginView();
             }
-            return const Text("done");
-            
-           
+            //  else {
+            //    return const LoginView();
+            // }
+            return const NotesView();
+
           default:
             return const CircularProgressIndicator();
         }
       },
     );
   }
+}
+
+class NotesView extends StatefulWidget {
+  const NotesView({Key? key}) : super(key: key);
+
+  @override
+  State<NotesView> createState() => _NotesViewState();
+}
+
+enum MenuAction { logout }
+
+class _NotesViewState extends State<NotesView> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(' Main UI'),
+        actions: [
+          PopupMenuButton<MenuAction>(
+            onSelected: (value) async {
+              switch (value) {
+                case MenuAction.logout:
+                  final shouldLogout = await showLogOutDialog(context);
+                  if (shouldLogout) {
+                    await FirebaseAuth.instance.signOut();
+                    Navigator.of(context)
+                        .pushNamedAndRemoveUntil('/login/', (_) => false);
+                  }
+                  break;
+              }
+            },
+            itemBuilder: (context) {
+              return const [
+                PopupMenuItem<MenuAction>(
+                  value: MenuAction.logout,
+                  child: Text('Log Out'),
+                ),
+              ];
+            },
+          )
+        ],
+      ),
+      body: const Text('Hello World'),
+    );
+  }
+}
+
+Future<bool> showLogOutDialog(BuildContext context) {
+  return showDialog<bool>(
+    context: context,
+    builder: (context) {
+      return AlertDialog(
+          title: const Text('sign out'),
+          content: const Text('Are you sure you want to sign out?'),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop(false);
+              },
+              child: const Text('Cancel'),
+            ),
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop(true);
+              },
+              child: const Text('Log out'),
+            ),
+          ]);
+    },
+  ).then((value) => value ?? false);
 }
--- lib/views/register_view.dart
+++ lib/views/register_view.dart
@@ -1,7 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-
-
+import 'package:flutter_progress/views/verify_email_view.dart';
 
 class RegisterView extends StatefulWidget {
   const RegisterView({Key? key}) : super(key: key);
@@ -58,6 +58,16 @@ class _RegisterViewState extends State<RegisterView> {
             onPressed: () async {
               final email = _email.text;
               final password = _password.text;
+              // try{
+              //   if (user.emailVerified) {
+              //   print('email is verified');
+              // } else {
+              //   return const VerifyEmailView();
+              // }
+              // } catch{
+
+              // }
+
               try {
                 final userCredential = await FirebaseAuth.instance
                     .createUserWithEmailAndPassword(
@@ -71,6 +81,7 @@ class _RegisterViewState extends State<RegisterView> {
                 } else {
                   print(e.code);
                 }
+               // FirebaseAuth.userCredential.reload();
               }
             },
             child: const Text("Register"),
